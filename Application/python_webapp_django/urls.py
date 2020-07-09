c8967f1ebb78d3a6e8e49f2d9213d66ef38222f9
Suyash Saluja <susaluj@microsoft.com>
2020-07-09 15:36:40 +0530

    urls

--- Application/python_webapp_django/urls.py
+++ Application/python_webapp_django/urls.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from django.conf.urls import url
 import django.contrib.auth.views
 from django.views.static import serve
+from django.conf import settings
 
 import app.forms
 import app.views
