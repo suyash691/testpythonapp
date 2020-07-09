678d77b38144246253ff9616ce5d48bc9cbbb09b
Suyash Saluja <susaluj@microsoft.com>
2020-07-09 15:49:42 +0530

    static path

--- Application/python_webapp_django/settings.py
+++ Application/python_webapp_django/settings.py
@@ -123,10 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(os.path.join(BASE_DIR,'app'), 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR,'static'),
+    os.path.join(os.path.join(BASE_DIR,'app'), 'static'),
 )
 
 STATICFILES_FINDERS = (
